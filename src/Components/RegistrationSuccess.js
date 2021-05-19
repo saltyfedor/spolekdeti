dd841c3b28c6e6617a5dec3641d44c3bcd83c9fc
saltyfedor <fedor.stryapunin@gmail.com>
2021-05-19 19:14:29 +0200

    registration success button

--- src/Components/RegistrationSuccess.js
+++ src/Components/RegistrationSuccess.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
+import { Link } from "react-router-dom";
+
 const RegistrationSuccess = () => {
 
     return (
@@ -7,8 +9,9 @@ const RegistrationSuccess = () => {
         <div className="registration-outer">
             <div className="registration-inner">
                 <div className="success-message-container">
-                    <h2 className="camp-section-title">Úspěšné přihlášení!</h2>
-                    <p className="tc">Potvrzení, podrobné informace o platbě, dokumenty k vytsiknutí a jiné instrukce zašleme na váš email.</p>
+                    <h2 className="camp-section-title mb20 mt0">Úspěšné přihlášení!</h2>
+                        <p className="tc mv20">Potvrzení, podrobné informace o platbě, dokumenty k vytsiknutí a jiné instrukce zašleme na váš email.</p>
+                        <div className="sign-up-button db back-button mt20"><Link to={`/`} style={{ textDecoration: 'none', display: "inline-block" , color: "#FFFFFF"}}>Zpět na hlavní</Link></div>
                 </div>
                
             </div>
