68faa725cba0c4b8e2becc2c11980fc1d48798c9
saltyfedor <fedor.stryapunin@gmail.com>
2021-10-27 18:29:40 +0200

    updated contacts

---
+++ src/Components/CampLeads/CampLeads.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const CampLeads = () => { }
+
+export default CampLeads
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,11 +18,12 @@ const Footer = () => {
                 <h3 className="footer-copy">©2021 AGENTURA SPOLEK DĚTÍ</h3>
                 <div className="contact-text">
                     <h3 id="kontakt" className="footer-copy">KONTAKT</h3>
-                    <p className="contact-line">Agentura spolek děti</p>
+                    <p className="contact-line">spoldeti, s.r.o.</p>
                     <p className="contact-line">Email: spolekdeti@seznam.cz</p>
                     <p className="contact-line">Telefon: +420 608 159 466</p>
-                    <p className="contact-line">Adresa: Raisová 613, Jičín, 50601</p>
-                    <p className="contact-line">Číslo účtu: 3042393053/0800</p>
+                    <p className="contact-line">IČO: 11931451</p>
+                    <p className="contact-line">Sídlo společnosti: Praha 10 - Korunní 2569/108 PSČ: 10100</p>
+                    <p className="contact-line">Číslo účtu: 5949212002/5500</p>
                 </div>               
             </div>
         
