89036f62b71583808b26cfa2cdced7a85306234a
saltyfedor <fedor.stryapunin@gmail.com>
2021-10-31 00:12:57 +0200

    added animations

--- src/Components/Team/TeamCard.js
+++ src/Components/Team/TeamCard.js
@@ -1,10 +1,13 @@
-import React,{ useState} from 'react'
+import React,{ useState, useEffect} from 'react'
 import fallback from '../../Images/fallback.jpg'
 import instaIcon from '../../Images/icons/instagram.svg'
 import tiktokIcon from '../../Images/icons/tiktok.svg'
+import { CSSTransition } from 'react-transition-group'
 import apiAdress from '../Variables'
 
 const TeamCard = ({ data }) => {   
+    const [showDesc, updateShowDesc] = useState(false)
+    const [currentClasses, updateCurrentClasses] = useState('')    
     
     const backgroundImage = `${apiAdress}images/team/squared/${data.link}`
     
@@ -37,17 +40,51 @@ const TeamCard = ({ data }) => {
             )
         }
         else return null
+    }   
+
+    const handleButtonClick = () => {
+       updateShowDesc(!showDesc)
     }
 
+    const getButtonText = () => {
+        if (!showDesc) return "O MNĚ"
+        else return "SKRÝT"
+    }
+
+    const getClasses = () => {
+        if (showDesc) {
+            return 'no-vis'
+        }
+    }
+   
+
     return (
-        <div className="team-card-container">
+        <div className={`team-card-container`}>
+            <div className="info-wrapper  team-flex-col">
+            <div> 
             <div className="member-image-wrapper">
-                <img className="member-image" src={backgroundImage} alt="obrazek-vedouciho" onError={(e)=>{e.target.onerror = null; e.target.src=fallback}}/>
+                <img className="member-image" src={backgroundImage} alt="obrazek-vedouciho" onError={(e) => { e.target.onerror = null; e.target.src = fallback }} />
             </div>
-            <h2 className=" member-name tc c-blue mt25">{data.name}</h2>
-            <h3 className="member-role tc">{getRole()}</h3>
-            {data.additional === "influencer" ? getSocial() : null}                            
-            <p className="team-card-text mb0 tc mt10">{data.description}</p>           
+            
+                <h2 className=" member-name tc c-blue mt25">{data.name}</h2>
+                <h3 className="member-role tc">{getRole()}</h3>
+                {data.additional === "influencer" ? getSocial() : null}
+            </div> 
+                <div className={`team-button tc mt20 ${currentClasses}`} onClick={handleButtonClick}>O MNĚ</div>
+            </div>    
+            <CSSTransition
+                        in={showDesc}
+                        timeout={500}
+                        classNames="long-desc"
+                        unmountOnExit                        
+                    >
+                        <div className="open-container">
+                            <h2 className=" member-name tc c-blue mt0">{data.name}</h2>
+                            <p className="team-card-text m0 tc bt-blue">{data.description}</p>
+                            <div className="team-button tc mt20" onClick={handleButtonClick}>{getButtonText()}</div>
+                        </div>
+            </CSSTransition>
+            
         </div>
     )
 }
