45d1d95303d42d33978e669a510df9de7e9b2433
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-22 19:14:48 +0100

    fixed gallery

--- src/Components/Locale/LocaleGallery.js
+++ src/Components/Locale/LocaleGallery.js
@@ -2,24 +2,15 @@ import React from "react";
 import pecka1 from '../../Images/pecka1.jpg'
 import pecka2 from '../../Images/pecka2.jpg'
 import pecka3 from '../../Images/pecka3.jpg'
+import pecka4 from '../../Images/pecka4.JPG'
 import './LocaleGallery.css'
 
-const images = [pecka1, pecka2, pecka3]
-
 const LocaleGallery = () => {
-
-    const getImages = () => {
-        const imgList = images.map(img => {
-            return (
-                <img className="locale-galley-img" alt="obrázek z lokality" src={img} />
-            )
-        })
-        return imgList
-    }
-
+   
     return (
-        <div className="locale-galley-wrapper">
-            {getImages()}
+        <div className="locale-galley-wrapper">           
+                <img className="locale-galley-img" alt="obrázek z lokality" src={pecka1} />
+                <img className="locale-galley-img" alt="obrázek z lokality" src={pecka2} />            
         </div>
     )
 }
