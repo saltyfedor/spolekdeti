8fcc59db11b89fdcbfb04c940f82330d9d65389d
saltyfedor <fedor.stryapunin@gmail.com>
2021-11-23 20:17:23 +0100

    fixed contact page

--- src/Components/ContactPage/Contact.js
+++ src/Components/ContactPage/Contact.js
@@ -38,6 +38,7 @@ const Contact = () => {
             <div className="contact-display">
                     {orgData? getOrgCards(orgData) : null}                    
                 </div>
+            
             <h1 className="home-page-title mtres">KONTAKTUJTE NÁS</h1>
             <div className="contact-card">
                 <h3>Telefon: +420 608 159 466</h3>
@@ -45,11 +46,14 @@ const Contact = () => {
                 <h3>Sídlo společnosti: Praha 10 - Korunní 2569/108 PSČ: 10100</h3>
                 <h3>IČO: 11931451</h3>
                 <h3>Bankovní spojení: 5949212002/5500</h3>
-            </div>
-            
+            </div>    
         </div>
+        
     )
 
 }
 
 export default Contact
+
+/*
+*/
