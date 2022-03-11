57c8bce28005650ed1148ae70e6101284a9ad34c
saltyfedor <fedor.stryapunin@gmail.com>
2022-03-11 23:48:42 +0100

    added social media icons

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../../index.css'
 import './Nav.css'
+import contact from '../../Images/icons/contact-round.svg'
+import fb from '../../Images/icons/facebook-round.svg'
+import ig from '../../Images/icons/instagram-round.svg'
 import {Link} from 'react-router-dom'
 import logo from '../../Images/logo_small.png'
 
@@ -11,8 +14,10 @@ const Navbar = () => {
             <Link to={`/`} style={{ textDecoration: 'none' }}>
                 <img src={logo} alt='logo' className="logo-top"></img>
             </Link>            
-            <div className="navbar-links">                
-                <a href="kontakt" className="nav-link"><h4 style={{margin:0}}>KONTAKT</h4></a>
+            <div className="navbar-links">
+                <a href='https://www.facebook.com/agenturaspolekdeti/'><img className="nav-icon mr15" src={fb} alt=""></img></a>
+                <a href='https://www.instagram.com/spolekdeti/'><img className="nav-icon mr15" src={ig} alt=""></img></a>
+                <Link to={`/kontakt`}><img className="nav-icon mr15" src={contact} alt=""></img></Link>
             </div>
         </div>
     )
