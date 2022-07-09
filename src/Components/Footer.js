6dbe4a5070ca49807ea122bcc9e1fdb4d93bb66f
saltyfedor <fedor.stryapunin@gmail.com>
2022-07-09 18:47:27 +0200

    charity added logo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import logo from '../Images/diecezni_charita_hradec-kralove_BARVA_rgb.jpg'
 
 
 const Footer = () => {    
@@ -9,7 +9,8 @@ const Footer = () => {
             <div className="footer">               
                 <h3 className="footer-copy">©2021 AGENTURA SPOLEK DĚTÍ</h3>
                 <div className='footer-partner-container'>
-                    <a className='mr10-r footer-partner-href' href="http://detske-tabory.info/" title="Letní, podzimní, zimní i jarní dětské tábory - akce pro děti na prázdniny">
+                    <img src={logo} className="footer-partner-image footer-partner-href" width="88" height="31" alt="some-logo"/>
+                    <a className='footer-partner-href' href="http://detske-tabory.info/" title="Letní, podzimní, zimní i jarní dětské tábory - akce pro děti na prázdniny">
                         <img src="http://detske-tabory.info/zvyhodneni/organizace?size=88x31&amp;ic=11931451" alt="Letní, podzimní, zimní i jarní dětské tábory - akce pro děti na prázdniny" width="88" height="31" />
                     </a>
                     <a href="http://www.taboreni.cz/?zdroj=7900" title="Dětský tábor">
